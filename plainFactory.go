b2d7a0c6521f8010de9006275afe92903c07c6df
Manuel Valls <manolo@vlrz.es>
2018-07-09 14:40:53 +0200

    Move hfc into main package

--- plainFactory.go
+++ plainFactory.go
@@ -1,11 +1,10 @@
-package hfc
+package wit
 
 import (
 	"bytes"
 	"io"
 	"strings"
 
-	"github.com/manvalls/wit"
 	"golang.org/x/net/html"
 )
 
@@ -28,26 +27,26 @@ func (f *plainFactory) Nodes(ctx *html.Node) []*html.Node {
 
 // FromReaderFactory returns a plain HTMLFactory from a function expected to
 // return valid HTML
-func FromReaderFactory(factory func() io.Reader) wit.HTMLFactory {
+func FromReaderFactory(factory func() io.Reader) HTMLFactory {
 	return &plainFactory{factory}
 }
 
 // FromString returns a plain HTMLFactory from a string
-func FromString(html string) wit.HTMLFactory {
+func FromString(html string) HTMLFactory {
 	return &plainFactory{func() io.Reader {
 		return strings.NewReader(html)
 	}}
 }
 
 // FromBytes returns a plain HTMLFactory from a byte slice
-func FromBytes(html []byte) wit.HTMLFactory {
+func FromBytes(html []byte) HTMLFactory {
 	return &plainFactory{func() io.Reader {
 		return bytes.NewReader(html)
 	}}
 }
 
 // FromHandler returns a plain HTMLFactory from a handler function
-func FromHandler(handler func(io.Writer)) wit.HTMLFactory {
+func FromHandler(handler func(io.Writer)) HTMLFactory {
 	return &plainFactory{func() io.Reader {
 		r, w := io.Pipe()
 
